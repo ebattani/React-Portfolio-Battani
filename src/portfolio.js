32c6fdfdbecd3655d33cdd3fecc3ad2a9112e2be
Ethan Battani <battaniethan@gmail.com>
2022-06-03 11:15:44 -0600

    init

---
+++ src/portfolio.js
@@ -0,0 +1,60 @@
+const header = {
+
+  homepage: 'https://github.com/ebattani',
+  title: 'SooSpace',
+}
+
+const about = {
+ 
+  name: 'Ethan Battani',
+  role: 'Full Stack Engineer',
+  description:
+    'Hi, Im Ethan. A full stack developer living in Denver Colorado. I recently moved here with the intention of creating a new life and new career path for myself. Feel free to browse and check out some of my recent projects below. I graduated from Grand Valley State University with a Bachelors in Public and Non-Profit Administration and a Business minor. My intention was to work for a non profit organization with the intention of creating and running my own. While running an organization is still a goal of mine. The tech industry provides more opportunity and work-life balance.',
+  resume: 'https://drive.google.com/file/d/1MrAySzOAume2v_CCLvBo1A6bmjkkpZ-y/view?usp=sharing',
+  social: {
+    linkedin: 'https://www.linkedin.com/in/ethan-battani-92158264/',
+    github: 'https://github.com/ebattani',
+  },
+}
+
+const projects = [
+
+  {
+    name: 'E-Commerce Back End',
+    description:
+      'An E-Commerce Website with a focus on the backend development',
+    sourceCode: 'https://github.com/ebattani/Ecommerce-Backend-Battani',
+  },
+  {
+    name: 'Financial Statement Ratio-Analysis',
+    description:
+      'An application that is used to prepare visual representations to compare the strength of different companies',
+    sourceCode: 'https://github.com/ebattani/Financial-Statement-Ratio-Analysis',
+    livePreview: 'https://fsa-ratio.herokuapp.com/',
+  },
+  {
+    name: 'Text Editor',
+    description:
+      'A text editor application',
+    stack: ['TypeScript'],
+    sourceCode: 'https://github.com/ebattani/PWA-Text-Editorr-Battani',
+    livePreview: 'https://pwatexteditorbattani.herokuapp.com/',
+  },
+]
+
+const skills = [
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'TypeScript',
+  'React',
+  'Redux',
+  'Git'
+]
+
+const contact = {
+
+  email: 'battaniethan@gmail.com',
+}
+
+export { header, about, projects, skills, contact }
